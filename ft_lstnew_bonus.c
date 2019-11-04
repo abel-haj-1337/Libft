a30abb234b387d0322f365806ce268fb4abea5a3
Abdelaziz Bel haj <abel-haj@e2r9p4.1337.ma>
2019-11-04 01:11:18 +0100

    fixed (hopefully) bonus functions file names

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abel-haj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/21 15:50:13 by abel-haj          #+#    #+#             */
+/*   Updated: 2019/10/21 18:53:31 by abel-haj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*list;
+
+	list = malloc(sizeof(t_list));
+	if (list)
+	{
+		list->content = content;
+		list->next = NULL;
+	}
+	return (list);
+}
